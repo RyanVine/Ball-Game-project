5373a21dd56f733cf2442a2fd9463185ba928506
Ryan Vine <R.vine66@gmail.com>
2022-06-13 17:14:06 +0100

    removed bug & set ball area limitations

--- scripts/dist/app.dev.js
+++ scripts/dist/app.dev.js
@@ -1,40 +1,44 @@
+"use strict";
+
 //Variables
 var interval;
-let both = 0;
-//Query Selectors
-const player = document.querySelector(".game__player");
-const game = document.querySelector(".game");
-//Movement Functions
+var both = 0; //Query Selectors
+
+var player = document.querySelector(".game__player");
+var game = document.querySelector(".game"); //Movement Functions
+
 function movementLeft() {
   var left = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+
   if (left > 0) {
     player.style.left = left - 2 + "px";
   }
 }
 
 function movementRight() {
-  const left = parseInt(
-    window.getComputedStyle(player).getPropertyValue("left"));
-    if(left<380){
-    player.style.left = left + 2 + "px";
-   }
-}
+  var left = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
 
-//Event Listener for motion
-document.addEventListener("keydown", (event) => {
+  if (left < 380) {
+    player.style.left = left + 2 + "px";
+  }
+} //Event Listener for motion
+
+
+document.addEventListener("keydown", function (event) {
   if (both == 0) {
     both++;
+
     if (event.key === "ArrowLeft") {
       interval = setInterval(movementLeft, 1);
     }
+
     if (event.key === "ArrowRight") {
       interval = setInterval(movementRight, 1);
     }
   }
-});
+}); //Clearing the Event listener
 
-//Clearing the Event listener
-document.addEventListener("keyup", (event) => {
+document.addEventListener("keyup", function (event) {
   clearInterval(interval);
   both = 0;
 });
